7e7c49b7163ea158b49e5f8004096e1972b17e51
Douglas Crafts <Doug@FreshProgramming.com>
2016-10-29 03:10:06 -0500

    Added stopwatch to track playtime

--- gameCode/Game.js
+++ gameCode/Game.js
@@ -90,6 +90,7 @@ var totalLinesCleared = 0;
 var level = 1;
 var tetrises = 0;
 var scoredTetrisLast = false;
+var stopwatch = new Stopwatch();
 
 function gameBootstrap()
 {	
@@ -196,20 +197,28 @@ function processInput(time)
 	if(currentGameInput.DownPressed)   gameInput.DownUnHandled = true;
 	if(currentGameInput.LeftPressed && !gameInput.LeftPressed)   gameInput.LeftUnHandled = true;
 	if(currentGameInput.RightPressed && !gameInput.RightPressed) gameInput.RightUnHandled = true;
-	if(currentGameInput.PausePressed && !gameInput.PausePressed) gameInput.PauseUnHandled = true;
+	if(currentGameInput.PausePressed && !gameInput.PausePressed) gamePause();
 	if(currentGameInput.SoundKeyPressed && !gameInput.SoundKeyPressed) toggleSound();
 	if(currentGameInput.RotLeftPressed && !gameInput.RotLeftPressed) gameInput.RotLeftUnHandled = true;
 	if(currentGameInput.RotRightPressed && !gameInput.RotRightPressed) gameInput.RotRightUnHandled = true;
 	if(currentGameInput.DropPressed && !gameInput.DropPressed) gameInput.DropUnHandled = true;
 	
-	if(gameInput.PauseUnHandled)
+	gameInput.updatePressed(currentGameInput);
+}
+
+function gamePause()
+{
+	if(gameState==GameState.Playing || gameState==GameState.Animating)
 	{
 		gamePaused = !gamePaused;
+		if(gamePaused)
+			stopwatch.stop();
+		else
+			stopwatch.start();
+		
 		toggleMusicPause();
-		gameInput.PauseUnHandled = false;
 		gameInput.clearKeys();//clear keys pressed while paused
 	}
-	gameInput.updatePressed(currentGameInput);
 }
 
 function gameStart()
@@ -226,6 +235,8 @@ function gameStart()
 	clearBoard();
 	resetScore();
 	gameState = GameState.Playing;
+	stopwatch.reset();
+	stopwatch.start();
 	
 	playBackgroundMusic();
 	
@@ -386,6 +397,7 @@ function snapPiece()
 
 function gameOver()
 {
+	stopwatch.stop();
 	gamePaused = true;
 	gameState = GameState.GameOver;
 	toggleMusicPause();
@@ -682,16 +694,16 @@ function drawFPS(context)
 	context.fillStyle = 'black';
 
 	var line = 0;
-	context.fillText("FPS:"+lastIntervalFPS+" - "+framesThisInterval,xPos,yPos+ySeperation*line++);
-	context.fillText("Mouse X:"+mousePos.X+" Y:"+mousePos.Y,         xPos,yPos+ySeperation*line++);
+	//context.fillText("FPS:"+lastIntervalFPS+" - "+framesThisInterval,xPos,yPos+ySeperation*line++);
+	context.fillText("FPS:"+lastIntervalFPS+" - "+stopwatch.formattedTime(),     xPos,yPos+ySeperation*line++);
 	context.fillText("Keys:"+keysPressed,        xPos,yPos+ySeperation*line++);
 	context.fillText("Input:"+gameInput,         xPos,yPos+ySeperation*line++);
-	context.fillText("paused:"+gamePaused,       xPos,yPos+ySeperation*line++);
+	context.fillText("State:"+gameState,         xPos,yPos+ySeperation*line++);
+	context.fillText("Sound:"+musicState,        xPos,yPos+ySeperation*line++);
+	line++;
 	context.fillText("Level:"+level,             xPos,yPos+ySeperation*line++);
 	context.fillText("Score:"+score,             xPos,yPos+ySeperation*line++);
 	context.fillText("Tetris:"+tetrises,         xPos,yPos+ySeperation*line++);
-	context.fillText("State:"+gameState,         xPos,yPos+ySeperation*line++);
-	context.fillText("Sound:"+musicState,         xPos,yPos+ySeperation*line++);
 	
 	
 	line = 0;
--- gameCode/GenericClasses.js
+++ gameCode/GenericClasses.js
@@ -1,20 +1,28 @@
 //functions
-function Create2DArray(rows) {
-  var arr = [];
+function Create2DArray(rows) 
+{
+	var arr = [];
 
-  for (var i=0;i<rows;i++) {
-     arr[i] = [];
-  }
+	for (var i=0;i<rows;i++)
+	{
+		arr[i] = [];
+	}
 
-  return arr;
+	return arr;
 }
 function getMousePos(canvas, evt) 
 {
 	var rect = canvas.getBoundingClientRect();
-	return {
-	  X: evt.clientX - rect.left,
-	  Y: evt.clientY - rect.top
+	return 
+	{
+		X: evt.clientX - rect.left,
+		Y: evt.clientY - rect.top
 	};
+}
+function pad(num, size) 
+{
+	var s = "0000" + num;
+	return s.substr(s.length - size);
 }
 //classes
 //Point
@@ -94,4 +102,65 @@ GameInput.prototype.toString=function()
 	if(this.DropPressed)result+="Drop,";
 	
 	return result;
+};
+//Stopwatch
+var	Stopwatch = function()
+{
+	// Private vars
+	var	startAt	= 0;	// Time of last start / resume. (0 if not running)
+	var	lapTime	= 0;	// Time on the clock when last stopped in milliseconds
+
+	var	now	= function()
+	{
+		return (new Date()).getTime();
+	};
+
+	// Public methods
+	// Start or resume
+	this.start = function()
+	{
+		startAt	= startAt ? startAt : now();
+	};
+
+	// Stop or pause
+	this.stop = function()
+	{
+		// If running, update elapsed time otherwise keep it
+		lapTime	= startAt ? lapTime + now() - startAt : lapTime;
+		startAt	= 0; // Paused
+	};
+
+	// Reset
+	this.reset = function()
+	{
+		lapTime = startAt = 0;
+	};
+
+	// Duration - total ms
+	this.time = function()
+	{
+		return lapTime + (startAt ? now() - startAt : 0);
+	};
+
+	// formatted time
+	this.formattedTime = function()
+	{
+		return this.formatTime(this.time());
+	};
+
+	this.formatTime = function(time)
+	{
+		var h = m = s = ms = 0;
+		var newTime = '';
+
+		h = Math.floor( time / (60 * 60 * 1000) );
+		time = time % (60 * 60 * 1000);
+		m = Math.floor( time / (60 * 1000) );
+		time = time % (60 * 1000);
+		s = Math.floor( time / 1000 );
+		ms = time % 1000;
+
+		newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+		return newTime;
+	}
 };
